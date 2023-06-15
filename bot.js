7e823758080d94e67cf8ca3826e3573919db8ae3
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2023-06-15 10:37:00 +0300

    change logic

--- bot.js
+++ bot.js
@@ -1,70 +1,39 @@
 // const bot = new Telegraf('5985563709:AAFqsAnRodXlOki_poTNAJym8d0tmevrX6c');
-const { Telegraf } = require('telegraf');
-const axios = require('axios');
+// const apiKey = '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'
+const Telegraf = require('telegraf');
 const openai = require('openai');
 
-const bot = new Telegraf('5985563709:AAFqsAnRodXlOki_poTNAJym8d0tmevrX6c');
-const apiKey = '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'
+const apiKey = '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'; // Ваш ключ API OpenAI
 
-bot.start((ctx) => ctx.reply('Привіт! Я бот для перевірки та виправлення граматичних помилок англійського тексту. Просто надішліть мені текст, і я перевірю та надам пропозиції щодо виправлень.'));
+const bot = new Telegraf('5985563709:AAFqsAnRodXlOki_poTNAJym8d0tmevrX6c'); // Ваш токен бота
+
+// Підключення до OpenAI API
+const openaiInstance = new openai.OpenAI(apiKey);
+
+bot.start((ctx) => ctx.reply('Привіт! Я бот для перевірки та виправлення граматичних помилок. Просто надішліть мені текст, і я перевірю та виправлю помилки.'));
 
 bot.on('text', async (ctx) => {
   const text = ctx.message.text;
 
-  const grammarErrors = await checkGrammar(text);
+  try {
+    const prompt = `In English, correct the grammar errors in the following text:\n${text}`;
 
-  let errorMessages = '';
+    const response = await openaiInstance.Completion.create({
+      engine: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 100,
+      temperature: 0.3,
+      n: 1,
+      stop: '\n',
+    });
 
-//   if (grammarErrors.length > 0) {
-//     const spellError = grammarErrors.map((error) => error.message)
-//     const correctedText = correctTextWithErrors(text, grammarErrors);
-//     const grammarSuggestions = grammarErrors.map((error) => `${error.message}`);
-//     errorMessages += `Знайдено граматичні помилки:\n\n${grammarSuggestions.join('\n')}\n\n`;
-//     if(spellError[0] !== 'Possible spelling mistake found.'){
-//         errorMessages += `Ось виправлений текст:\n\n${correctedText}👌\n`;
-//     }
-//   } else {
-//     errorMessages += 'Помилок у граматиці не знайдено. Ваш текст вірний!';
-//   }
+    const correctedText = response.choices[0].text.trim();
 
-  ctx.reply(grammarErrors);
+    ctx.reply(`Знайдено помилки. Ось виправлений текст:\n\n${correctedText}`);
+  } catch (error) {
+    console.error('Error occurred during grammar correction:', error);
+    ctx.reply('Виникла помилка при виправленні граматики. Будь ласка, спробуйте ще раз.');
+  }
 });
-
-async function checkGrammar(text) {
-    try {
-        const openaiInstance = new openai.OpenAI(apiKey);
-    
-        const prompt = `In English, correct the grammar errors in the following text:\n${text}`;
-    
-        const response = await openaiInstance.Completion.create({
-          engine: 'text-davinci-003',
-          prompt: prompt,
-          max_tokens: 100,
-          temperature: 0.3,
-          n: 1,
-          stop: '\n',
-        });
-    
-        const correctedText = response.choices[0].text.trim();
-    
-        return correctedText;
-      } catch (error) {
-        console.error('Error occurred during grammar correction:', error);
-        return []; // Повернення оригінального тексту у випадку помилки
-      }
-}
-
-function correctTextWithErrors(text, grammarErrors) {
-    let correctedText = text;
-  
-    for (let i = grammarErrors.length - 1; i >= 0; i--) {
-      const { offset, length, replacements } = grammarErrors[i];
-      const replacement = replacements[0]?.value || '';
-  
-      correctedText = correctedText.slice(0, offset) + replacement + correctedText.slice(offset + length);
-    }
-  
-    return correctedText;
-}
 
 bot.launch();
